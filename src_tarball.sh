49e1059aa07216ce16b357c8e1eed0a1956205f6
Ken Rice <krice@freeswitch.org>
2015-05-11 16:25:38 -0500

    make the patch right

--- src_tarball.sh
+++ src_tarball.sh
@@ -7,7 +7,7 @@ TMPDIR=libsilk.$$
 mkdir -p ../${TMPDIR}
 
 cd ..
-cp -a liblibsilk ${TMPDIR}/libsilk-1.0.8
+cp -a libsilk ${TMPDIR}/libsilk-1.0.8
 cd ${TMPDIR}
 rm -rf libsilk-1.0.8/.git*
 tar zcvf libsilk-1.0.8.tar.gz libsilk-1.0.8
